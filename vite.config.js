27d54c746f542b31bbebebbb5bc93d84011791d8
weedlesniper <lucadms21@gmail.com>
2025-12-03 10:43:13 +0800

    minor: font size change for readability

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
+import tailwindcss from '@tailwindcss/vite';
 import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+
 
 export default defineConfig({
     plugins: [
